d76e6881e91a19f3bc01b157ced5fbb29ff300f6
Wise Duho <duhowise@gmail.com>
2022-01-13 21:43:35 +0000

    Add project files.

---
+++ CompanyEmployees/Program.cs
@@ -0,0 +1,42 @@
+using CompanyEmployees.Extensions;
+using Contracts;
+using LoggerService;
+using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
+
+
+var builder = WebApplication.CreateBuilder(args);
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+// Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerService();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("CorsPolicy");
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
