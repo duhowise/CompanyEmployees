d5ad701a236c41220fac3e1444eb2f358867c579
Wise Duho <duhowise@gmail.com>
2022-01-17 22:07:29 +0000

    completed get employees for companies

---
+++ CompanyEmployees/Controllers/EmployeesController.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Contracts;
+using Entities.DataTransferObjects;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyEmployees.Controllers
+{
+    [Route("api/employees")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IRepositoryManager _repository;
+        private readonly ILoggerManager _logger;
+        private readonly IMapper _mapper;
+
+        public EmployeesController(IRepositoryManager repository, ILoggerManager logger, IMapper mapper)
+        {
+            _repository = repository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetEmployeesForCompany(Guid companyId)
+        {
+            var company = _repository.Company.GetCompany(companyId, false);
+            if (company == null)
+            {
+                _logger.LogInfo($"Company with Id: {companyId} does not exist");
+                return NotFound();
+            }
+
+            var employeesFromDb = _repository.Employee.GetEmployees(companyId, trackChanges: false);
+            var employeesDto = _mapper.Map<EmployeeDto[]>(employeesFromDb);
+            return Ok(employeesFromDb);
+        }
+    }
+}
--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -6,7 +6,15 @@ namespace Repository;
 
 public class EmployeeRepository:RepositoryBase<Employee>,IEmployeeRepository
 {
+    private readonly RepositoryContext _context;
+
     public EmployeeRepository(RepositoryContext context) : base(context)
     {
+        _context = context;
+    }
+
+    public IEnumerable<Employee> GetEmployees(Guid companyId, bool trackChanges)
+    {
+        return FindByCondition(x => x.CompanyId.Equals(companyId), trackChanges).OrderBy(x=>x.Name);
     }
 }
